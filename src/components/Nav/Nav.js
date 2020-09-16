fcb56272277a106cff4f3edc930f63fb81045ed2
1voyager3 <1voyager3@gmail.com>
2020-09-16 11:36:33 +0100

    Add update nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,8 +5,12 @@ function Nav ({prevCalendar, nextCalendar}) {
 
     return (
         <div className="Nav">
-            <a href="#" className="prev" onClick={prevCalendar}>←</a>
-            <a href="#" className="next" onClick={nextCalendar}>→</a>
+            <a href="" className="prev" onClick={prevCalendar}>
+                <button>←</button>
+            </a>
+            <a href="" className="next" onClick={nextCalendar}>
+                <button>→</button>
+            </a>
         </div>
     )
 }
